a400fb170f412753f59771fca25973acd8a32c57
Nico0084 <nico84dev@gmail.com>
2015-11-20 15:31:14 +0100

    Fix thermostatSetPoint, remove log debug

--- lib/ozwnode.py
+++ lib/ozwnode.py
@@ -1063,10 +1063,9 @@ class ZWaveNode:
                     self.log.warning(u"Unkwon command select type {0}".format(params))
             elif command == 'temperature' :
                 cmdsClass = ['COMMAND_CLASS_THERMOSTAT_SETPOINT']
-                self.log.debug("Enter in setpoint {0} instance {1}".format(cmdsClass, instance))
+                sended = False
                 for value in self.values.keys() :
                     val = self.values[value].valueData
-                    self.log.debug("search in value : {0} instance {1} label {2}".format(val['commandClass'], val['instance'], self.values[value].labelDomogik))
 #                  Handle Thermostat setpoint type 'setpoint' to :
 #                          'unused-0', 'heating-1', 'cooling-1', 'unused-3', 'unused-4', 'unused-5', 'unused-6', 'furnace',
 #                          'dry-air', 'moist-air', 'auto-changeover', 'heating-econ', 'cooling-econ','away-heating'
@@ -1074,8 +1073,8 @@ class ZWaveNode:
                     if (val['commandClass'] in cmdsClass) and val['instance'] == instance and \
                         self.values[value].labelDomogik in ['unused-0', 'heating-1', 'cooling-1', 'unused-3', 'unused-4', 'unused-5', 'unused-6',
                                                             'furnace', 'dry-air', 'moist-air', 'auto-changeover', 'heating-econ', 'cooling-econ','away-heating']:
-                        self.log.debug("valeur Identifiée comme type setpoint : {0}".format(val))
                         retval = self.values[value].setValue(params['setpoint'])
+                        sended = True
                         break
                 if not sended :
                    self.log.warning("COMMAND_CLASS_THERMOSTAT_SETPOINT temperature setpoint for nodeId {0} instance {1} no find value for label {2}".format(self.nodeId, instance, self.values[value].labelDomogik))
