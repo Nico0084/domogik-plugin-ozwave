dd29395936ab654fb0797e61b8c78d8f86c4374c
Nico0084 <nico84dev@gmail.com>
2016-07-02 00:25:45 +0200

    Try travis with zwemulator and install dependcies plugin

---
+++ tests/travis/travis-install-plugindenpencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e 
+# The -e option will make the bash stop if any command raise an error ($? != 0)
+
+echo "==== Install zwave emulator dependency"
+sudo apt-get install socat
+cd $TRAVIS_BUILD_DIR
+git clone git@github.com:Nico0084/py-zwave-emulator.git
+export ZWEMULATOR = py-zwave-emulator/bin/zwemulator.py
+
+echo "==== Install Plugin dependency"
+./install_dependencies.sh -LAST
+
+echo "==== Start zwave emulator"
+python $ZWEMULATOR &
+
+
+
+
+
